1ec4ab61cc48c920d00e8ccb592d024bef1be62a
John West <contactjohnwest@gmail.com>
2025-06-01 17:07:15 -0700

    Added hugging face dependencies and removed generated config and lock files

--- server/index.js
+++ server/index.js
@@ -2,20 +2,32 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import stablediffusionRoutes from './routes/stableDiffusionRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.get('/', (req, res) => {
-  res.send('Hello from DALL-E!');
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/stablediffusionRoutes', stablediffusionRoutes);
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from Hugging Face!',
+  });
 });
 
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log('Server has started on port http://localhost:8080')
-  );
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log('Failed to connect to MongoDB:', error);
+  }
 };
 
 startServer();
